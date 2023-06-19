a49cfc0a20df0638c6c68a58bf542c5a0579a9fe
RenatoMor <renato.moreira@uni9.edu.br>
2023-06-19 12:05:21 -0300

    Projeto Pratico de Programação_config

---
+++ AZON/config.php
@@ -0,0 +1,18 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbpassword = 'Kontakt$23';
+    $dbname = 'formulario_rm'; 
+
+    $conexao = new mysqli($dbHost, $dbUsername, $dbpassword, $dbname);
+
+   //   if($conexao->connect_errno){
+   //      echo "Erro!";
+   //   }
+   //   else{
+   //      echo "Conexão efetuada com sucesso!";
+   //   } 
+    
+
+?>
